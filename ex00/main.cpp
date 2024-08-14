f75f4a860582b0ff7dfb15a795e9fa16d7e72451
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-14 16:23:35 +0200

    updated main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,7 +2,13 @@
 #include "ScalarConverter.hpp"
 
 int main(int argc, char **argv) {
-	(void)argc;	
-	(void)argv;	
+	std::string arg(argv[1]);
+	if (argc == 2) {
+		try {	
+			ScalarConverter::convert(arg);
+		} catch (std::exception &e) {
+			std::cout<<"Exception: "<<e.what()<<std::endl;
+		}
+	}
 	return (0);
 }

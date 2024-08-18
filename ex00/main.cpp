2719d0ae102ee8251744a7eac8835b7ba09a9f5a
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-19 00:32:07 +0200

    Added print usage if arg is not 1

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,13 +2,15 @@
 #include "ScalarConverter.hpp"
 
 int main(int argc, char **argv) {
-	std::string arg(argv[1]);
 	if (argc == 2) {
+		std::string arg = argv[1];
 		try {	
 			ScalarConverter::convert(arg);
 		} catch (std::exception &e) {
 			std::cout<<"Exception: "<<e.what()<<std::endl;
 		}
 	}
+	else 
+		std::cerr<<"Usage: ./convert <arg>"<<std::endl;
 	return (0);
 }

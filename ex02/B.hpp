03832b25f357669d05ccc403c3c01a3509414e95
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-08-19 15:05:53 +0200

    Removed Base header include

--- ex02/B.hpp
+++ ex02/B.hpp
@@ -2,8 +2,6 @@
 #ifndef B_HPP
 #define B_HPP
 
-#include "Base.hpp"
-
 class B : public Base {
 };
 
--- ex02/C.hpp
+++ ex02/C.hpp
@@ -2,8 +2,6 @@
 #ifndef C_HPP
 #define C_HPP
 
-#include "Base.hpp"
-
 class C : public Base {
 };
 

86b08693a291b647f079e8d45083f329efd79909
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-09-02 14:26:10 +0200

    replaced cout to cerr for exception print

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
 		try {	
 			ScalarConverter::convert(arg);
 		} catch (std::exception &e) {
-			std::cout<<"Exception: "<<e.what()<<std::endl;
+			std::cerr<<"Exception: "<<e.what()<<std::endl;
 		}
 	}
 	else 

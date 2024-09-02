3024c2ff0e7492cca8d96911e72d51c77cce165e
Sergiu Ster <ssergiu@student.42heilbronn.de>
2024-09-02 20:37:57 +0200

    removed unuset cout

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -13,7 +13,6 @@ int main(void) {
 	ptr = Serializer::serialize(&struct_data);
 	std::cout<<"normal struct: "<<struct_data.n<<std::endl;
 	std::cout<<"deserialized before: "<<deserialized_data.n<<std::endl;
-	std::cout<<"ptr: "<<ptr<<std::endl;
 	deserialized_data = *Serializer::deserialize(ptr);
 	std::cout<<"deserialized: "<<deserialized_data.n<<std::endl;
 	//struct_data = Serializer::deserialize(ptr);
